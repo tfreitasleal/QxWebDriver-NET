ee8e84b5df810b850e729129f016dfc303988a37
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-10-05 23:27:58 +0100

    Add test project for Wisej simple demo.

---
+++ Samples/Wisej.SimpleDemoTests/ApiViewerTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Wisej.WebDriver;
+using Wisej.WebDriver.UI;
+
+namespace Wisej.SimpleDemoTests
+{
+    public class ApiViewerTests
+    {
+        private QxWebDriver _driver;
+
+        [Test]
+        public void ChromeTest()
+        {
+            _driver = new QxWebDriver(new ChromeDriver());
+            DoTest();
+        }
+
+        [Test]
+        public void FirefoxTest()
+        {
+            _driver = new QxWebDriver(new FirefoxDriver());
+            DoTest();
+        }
+
+        public void DoTest()
+        {
+            // Open the page and wait until the qooxdoo application is loaded
+            _driver.Url = "http://www.qooxdoo.org/current/api/index.html";
+
+            // Find the 'Search' button in the tool bar
+            // @label is the button text
+            By buttonByLabel = By.Qxh("apiviewer.Viewer/*/[@label=Search]");
+            IWidget buttonWidget = _driver.FindWidget(buttonByLabel);
+
+            // Click the button if it's not already selected
+            if (!buttonWidget.Selected)
+            {
+                buttonWidget.Click();
+            }
+
+            // Now click the 'Legend' button
+            buttonByLabel = By.Qxh("apiviewer.Viewer/*/[@label=Legend]");
+            buttonWidget = _driver.FindWidget(buttonByLabel);
+            if (!buttonWidget.Selected)
+            {
+                buttonWidget.Click();
+            }
+
+            // Now click the 'Content' button
+            buttonByLabel = By.Qxh("apiviewer.Viewer/*/[@label=Content]");
+            buttonWidget = _driver.FindWidget(buttonByLabel);
+            if (!buttonWidget.Selected)
+            {
+                buttonWidget.Click();
+            }
+
+            // Select the "data" item from the package tree
+            By tree = By.Qxh("apiviewer.Viewer/*/apiviewer.ui.PackageTree");
+            ISelectable packageTree = (ISelectable) _driver.FindWidget(tree);
+            packageTree.SelectItem("data");
+        }
+    }
+}
---
+++ Samples/Wisej.SimpleDemoTests/ChromeTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Wisej.WebDriver;
+using Wisej.WebDriver.UI;
+using By = Wisej.WebDriver.By;
+
+namespace Wisej.SimpleDemoTests
+{
+    [TestFixture]
+    public class ChromeTests
+    {
+        private QxWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void OpenMainPage()
+        {
+            _driver = new QxWebDriver(new ChromeDriver());
+            _driver.Url = "http://localhost:16461/";
+        }
+
+       
+        [Test]
+        public void ElementExists()
+        {
+            IWebElement element = _driver.FindElement(By.Name("openWindow"));
+            Assert.IsNotNull(element);
+        }
+
+        [Test]
+        public void ButtonExists()
+        {
+            OpenQA.Selenium.By button = By.Name("openWindow");
+            IWidget openButton = _driver.FindWidget(button);
+            // Click the button if it's not already selected
+            if (!openButton.Selected)
+            {
+                openButton.Click();
+            }
+        }
+    }
+}
