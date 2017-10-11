e724c818674fe1cbe26a431b61f316d9690ed568
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-10-11 18:29:50 +0100

    Remove extension method FindElement.

--- Source/Wisej.Qooxdoo.WebDriver/WebDriverHelpers.cs
+++ Source/Wisej.Qooxdoo.WebDriver/WebDriverHelpers.cs
@@ -1,40 +1,7 @@
-﻿using System;
-using System.Linq;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
+﻿using System.Linq;
 
 namespace Wisej.Qooxdoo.WebDriver
 {
-    /// <summary>
-    /// WebDriver Extensions methods
-    /// </summary>
-    public static class WebDriverExtensions
-    {
-        /// <summary>
-        /// Find the first matching <seealso cref="IWebElement"/> using the given method.
-        /// </summary>
-        /// <param name="driver">The <seealso cref="IWebDriver"/> parameter.</param>
-        /// <param name="by">The locating mechanism to use.</param>
-        /// <param name="timeoutInSeconds"> time to wait for the element </param>
-        /// <returns>The first matching element on the current page.</returns>
-        /// <exception cref="NoSuchElementException"> If no matching element was found before the timeout elapsed </exception>
-        /// <seealso cref="By"/>
-        public static IWebElement FindElement(this IWebDriver driver, OpenQA.Selenium.By by, long timeoutInSeconds)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-            IWebElement element;
-            try
-            {
-                element = wait.Until(ExpectedConditions.ElementExists(by));
-            }
-            catch (WebDriverTimeoutException e)
-            {
-                throw new NoSuchElementException("Unable to find element for locator.", e);
-            }
-            return element;
-        }
-    }
-
     public partial class By
     {
         /// <summary>
