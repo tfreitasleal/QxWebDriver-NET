f3ba3121749a2b31593edab7384d924a1ccc1a5a
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-10-14 02:46:28 +0100

    Use Waiter for costly view updates: 500 ms in RELEASE mode and 2.000 ms in DEBUG mode.

--- Samples/SimpleDemo/SimpleDemo.MSTest/Waiter.cs
+++ Samples/SimpleDemo/SimpleDemo.MSTest/Waiter.cs
@@ -4,10 +4,10 @@
     {
 #if NOWAIT
         public static int Duration = 0;
-#elif RELEASE
-        public static int Duration = 500;
-#else
+#elif DEBUG
         public static int Duration = 2000;
+#else
+        public static int Duration = 500;
 #endif
     }
 }
