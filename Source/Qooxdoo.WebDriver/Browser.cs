9a942ed4604abe83c82bfe861957390e67a6e0fe
tfreitasleal <tfreitasleal@tfreitasleal.pt>
2017-10-26 02:41:59 +0100

    Code documentation.

--- Source/Qooxdoo.WebDriver/Browser.cs
+++ Source/Qooxdoo.WebDriver/Browser.cs
@@ -1,13 +1,43 @@
 ﻿namespace Qooxdoo.WebDriver
 {
+    /// <summary>
+    /// Supported browses enumeration used on <see cref="QxWebDriver"/> constructor.
+    /// </summary>
     public enum Browser
     {
+        /// <summary>
+        /// Chrome
+        /// </summary>
         Chrome,
+
+        /// <summary>
+        /// Microsoft Edge
+        /// </summary>
         Edge,
+
+        /// <summary>
+        /// Firefox
+        /// </summary>
         Firefox,
+
+        /// <summary>
+        /// Internet Explorer
+        /// </summary>
         IE,
+
+        /// <summary>
+        /// Opera
+        /// </summary>
         Opera,
+
+        /// <summary>
+        /// Phantom JS
+        /// </summary>
         PhantomJS,
+
+        /// <summary>
+        /// Safari
+        /// </summary>
         Safari
     }
 }
